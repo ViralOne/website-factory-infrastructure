1ab201a0835872e3faaf2a490dbd5054e0132e11
Mocanu Marian <viralnico@protonmail.com>
2022-05-07 22:05:08 +0300

    Create S3 backup

--- aws/s3/locals.tf
+++ aws/s3/locals.tf
@@ -5,11 +5,14 @@ locals {
   }
 
   bucket = {
-    description   = "Bucket details"
-    name          = "websitefactory-bucket" # change this to your bucket name
-    acl           = "public-read"
-    force_destroy = false
-    versioning    = true
-    mfa_delete    = false
+    description             = "Bucket details"
+    name                    = "websitefactory-bucket" # change this to your bucket name
+    acl                     = "public-read"
+    force_destroy           = false
+    versioning              = "Enabled"
+    sse_algorithm           = "AES256"
+    days_after_initiation   = 7
+    version_transition_days = 7
+    version_expiration_days = 30
   }
 }
--- aws/s3/s3.tf
+++ aws/s3/s3.tf
@@ -7,9 +7,44 @@ resource "aws_s3_bucket" "website-factory" {
     Name           = local.bucket.name
     Classification = "Public"
   }
+}
 
-  versioning {
-    enabled    = local.bucket.versioning
-    mfa_delete = local.bucket.mfa_delete
+resource "aws_s3_bucket_versioning" "website-factory" {
+  bucket = aws_s3_bucket.website-factory.id
+
+  versioning_configuration {
+    status = local.bucket.versioning
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "website-factory" {
+  bucket = aws_s3_bucket.website-factory.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = local.bucket.sse_algorithm
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "website-factory" {
+  bucket = aws_s3_bucket.website-factory.id
+
+  rule {
+    status = "Enabled"
+    id     = "noncurrent_transition"
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = local.bucket.days_after_initiation
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = local.bucket.version_transition_days
+      storage_class   = "STANDARD_IA"
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = local.bucket.version_expiration_days
+    }
   }
 }
