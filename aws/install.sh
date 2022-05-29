7157f67b33e61b8106e979ac9ed4c3b3d709518d
Mocanu Marian <viralnico@protonmail.com>
2022-05-29 15:32:35 +0000

    Update install.sh

--- aws/install.sh
+++ aws/install.sh
@@ -14,26 +14,6 @@ newgrp docker
 sudo snap disable docker
 sudo snap enable docker
 
-# Install Docker
-    # sudo apt-get install -y \
-    #     apt-transport-https \
-    #     ca-certificates \
-    #     curl \
-    #     software-properties-common
-
-    #  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-    # echo \
-    #   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    #   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    # sudo apt-get update
-    # sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-    # sudo groupadd docker
-    # sudo usermod -aG docker $USER
-    # newgrp docker
-
 # Install Docker Compose
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
