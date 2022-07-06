3b041f4c700651283c25bf9799deb6a36a77864f
Mocanu Marian <mocanu.marian@agilefreaks.com>
2022-07-06 23:34:36 +0300

    Add postgresql server database

--- azure/db.tf
+++ azure/db.tf
@@ -1,18 +1,25 @@
 # Create postgresql database
 resource "azurerm_postgresql_flexible_server" "websitefactory_db_server" {
   name                = local.db_config.name
-  location            = "Sweden Central"
+  location            = "East US"
   resource_group_name = azurerm_resource_group.resource_group.name
 
   sku_name   = local.db_config.sku
   storage_mb = local.db_config.storage_mb
   
-  backup_retention_days         = local.db_config.backup_retention_days
-  geo_redundant_backup_enabled  = local.db_config.geo_redundant_backup_enabled
-  #public_network_access_enabled = local.db_config.public_network_access_enabled
+  backup_retention_days        = local.db_config.backup_retention_days
+  geo_redundant_backup_enabled = local.db_config.geo_redundant_backup_enabled
 
   administrator_login    = local.db_config.admin_user
   administrator_password = local.db_config.admin_pass
   version                = local.db_config.version
   zone                   = local.db_config.zone
+}
+
+# Manages a PostgreSQL Flexible Server DB
+resource "azurerm_postgresql_flexible_server_database" "server_database" {
+  name      = format("%s-%s", "db", local.tags.org_name)
+  server_id = azurerm_postgresql_flexible_server.websitefactory_db_server.id
+  collation = "en_US.utf8"
+  charset   = "utf8"
 }
