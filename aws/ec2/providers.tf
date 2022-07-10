7e42723bb8f9f4f1e58a962793569b21d6f99f0f
Mocanu Marian <viralnico@protonmail.com>
2022-07-10 16:39:35 +0300

    Update providers.tf

--- aws/ec2/providers.tf
+++ aws/ec2/providers.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-<<<<<<< HEAD:aws/rds/providers.tf
-      version = "4.16.0"
-=======
       version = "4.22.0"
->>>>>>> c4c5990a5e0567019b574deaf9b338917359f92e:aws/ec2/providers.tf
     }
   }
 }
